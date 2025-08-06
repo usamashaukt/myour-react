c89faab7567c61f0d99933d9ec732aeb9c2a3d71
usamashaukt <usamashaukat36@gmail.com>
2025-08-07 01:21:00 +0500

    changed link preview image

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0144f23f8e158a61"; }
+/******/ 		__webpack_require__.h = function() { return "df813e910ea95781"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ const Index = () => {
         <div className="background-filter">
           <div
             className="background-img"
-            style={{ backgroundImage: "url(images/bg-portfolio.jpg)" }}
+            style={{ backgroundImage: "url(images/bg-portfolio.webp)" }}
           />
         </div>
       </div>
