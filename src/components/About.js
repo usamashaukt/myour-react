933af152d8eb68f7949804ae92efc0c1de1476b2
usamashaukt <usamashaukat36@gmail.com>
2025-08-07 19:44:25 +0500

    changed link preview image

--- src/components/About.js
+++ src/components/About.js
@@ -14,11 +14,10 @@ const About = () => {
               <p>
                 Frontend-focused Full Stack Developer with 3+ years of experience building responsive, userfriendly
 
-                web and mobile applications. Proficient in React Native, React.js, Next.js, Vue.js, and
+                web and mobile applications. Proficient in  React.js, Next.js, Vue.js, and
                 Angular, with strong skills in creating scalable and maintainable frontend architectures,
                 implementing different design patterns, and integrating APIs. Experienced in integrating APIs with
-                Node.js and delivering custom solutions. Top Rated Plus freelancer on Upwork with 2000+ hours
-                logged, known for delivering high-quality, user-centric applications on time
+                Node.js, Next.js and delivering custom solutions. , user-centric applications on time
               </p>
             </div>
           </div>
